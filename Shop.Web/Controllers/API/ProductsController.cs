176ff8f0bf8e76fe64f190247912f97a0e9e3bff
neftalirodriguez <nefrc88@hotmail.com>
2020-06-15 20:43:56 -0500

    API Controllers

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        public IActionResult GetProducts() {
+            return this.Ok(this._productRepository.GetAll());
+        }
+    }
+}
